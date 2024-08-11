b2b57c210b11833238e346fcc8314e20e914457b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-11 13:28:00 +0500

    .

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./about.css"
+const About = () => {
+  return (
+    <div>
+      <div>About</div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./contact.css"
+const Contact = () => {
+  return (
+    <div>
+      <div>Contact</div>
+    </div>
+  )
+}
+
+export default Contact
