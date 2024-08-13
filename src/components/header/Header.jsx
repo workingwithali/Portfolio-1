5c3df00433afb1235c8d9dfdfa46dee494470fd5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-13 13:26:56 +0500

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,26 @@
 import "./header.css"
 import CTA from './CTA'
 import me from "../../assets/me.png"
+import HeaderSocials from "./HeaderSocials"
 const Header = () => {
   return (
-    <div>
-      <header>
-        <div className="container header_container">
-          <h5>Hello I'm</h5>
-          <h1>Ali Rashid</h1>
-          <h5 className="text-light">Fullstack Developer</h5>
-          <CTA/>
-          <div className="me">
-            <img src={me} alt="" />
-          </div>
-          <a href="#contact" className="scroll_down">Scroll Down</a>
 
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Ali Rashid</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={me} alt="" />
         </div>
-      </header>
-    </div>
+        <a href="#contact" className="scroll_down">Scroll Down</a>
+        <HeaderSocials/>
+      
+
+      </div>
+    </header>
+
   )
 }
 
--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
 
-const headerSocials = () => {
+
+
+const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://github.com/workingwithali"></a>
-        <a href="https://www.linkedin.com/in/workingwithali/"></a>
-        <a href="https://www.youtube.com/@workingwithali"></a>
-      
+        <a href="https://github.com/workingwithali" target='_blank'><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/workingwithali/" target='_blank'><FaLinkedin /></a>
+        <a href="https://www.youtube.com/@workingwithali" target='_blank'><FaYoutube /></a>
+       
     </div>
   )
 }
 
-export default headerSocials
+export default HeaderSocials
