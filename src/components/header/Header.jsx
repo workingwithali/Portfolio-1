16ac232d6cbf5edd0fa7c3f6519e71fe5fa671c9
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-13 12:10:24 +0500

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
 const Header = () => {
   return (
     <div>
@@ -8,7 +9,8 @@ const Header = () => {
           <h5>Hello I'm</h5>
           <h1>Ali Rashid</h1>
           <h5 className="text-light">Fullstack Developer</h5>
-          
+          <CTA/>
+
         </div>
       </header>
     </div>
