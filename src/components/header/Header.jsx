9882ecadc3e104fc2f59879e424c8709cd8d91b8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-13 11:52:00 +0500

    >

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import "./header.css"
 const Header = () => {
   return (
     <div>
-      <div>header</div>
+      <header>
+        <div className="container header_container">
+          <h5>Hello I'm</h5>
+          <h1>Ali Rashid</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+          
+        </div>
+      </header>
     </div>
   )
 }
