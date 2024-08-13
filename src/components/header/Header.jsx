5882d7d7335a04349a3bb862c942a368f5d701e1
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-13 12:35:48 +0500

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import "./header.css"
 import CTA from './CTA'
+import me from "../../assets/me.png"
 const Header = () => {
   return (
     <div>
@@ -10,6 +10,10 @@ const Header = () => {
           <h1>Ali Rashid</h1>
           <h5 className="text-light">Fullstack Developer</h5>
           <CTA/>
+          <div className="me">
+            <img src={me} alt="" />
+          </div>
+          <a href="#contact" className="scroll_down">Scroll Down</a>
 
         </div>
       </header>
---
+++ src/components/header/headerSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const headerSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://github.com/workingwithali"></a>
+        <a href="https://www.linkedin.com/in/workingwithali/"></a>
+        <a href="https://www.youtube.com/@workingwithali"></a>
+      
+    </div>
+  )
+}
+
+export default headerSocials
